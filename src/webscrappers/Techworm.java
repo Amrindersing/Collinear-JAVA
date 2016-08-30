ce27e75ae5d9a46e641ca249a82b35f018e0bcad
amrinder <iamdhindsa@gmail.com>
2016-08-30 21:51:34 +0530

    working on techworm scrapper

--- src/webscrappers/Techworm.java
+++ src/webscrappers/Techworm.java
@@ -1,19 +1,47 @@
 package webscrappers;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import DbHelper.Article;
 
 public class Techworm {
 
 	private static final String URL_PATH = "http://www.techworm.net/";
-	private Article article;
-	private 
+	private static Article article;
+	private static String data;
+	private static Document webpage;
+	private static Elements articles;
 	
-	public static String activateScrapper()
+	public static String activateScrapper() throws IOException
 	{
 		
-		Document doc = Jsoup.connect(URL_PATH).get();
+		webpage = Jsoup.connect(URL_PATH).get();
+		
+		articles = webpage.getElementsByTag("article");
+		
+		
+		
+		for (Element article : articles) {
+			System.out.println(article);
+			break;
+		}
 		
 		return null;
 		
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			Techworm.activateScrapper();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
